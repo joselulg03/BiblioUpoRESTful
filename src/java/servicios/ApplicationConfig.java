1e9f584e4850ca612ff678f09743e4fce99d5ddf
joslopez <joselopezguillen2003@gmail.com>
2024-06-04 19:03:48 +0200

    Servicios RESTFUL from database

--- src/java/servicios/ApplicationConfig.java
+++ src/java/servicios/ApplicationConfig.java
@@ -29,6 +29,20 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(entidades.service.AutorFacadeREST.class);
+        resources.add(entidades.service.CategoriaFacadeREST.class);
+        resources.add(entidades.service.EditorialFacadeREST.class);
+        resources.add(entidades.service.IdiomaFacadeREST.class);
+        resources.add(entidades.service.LibroFacadeREST.class);
+        resources.add(entidades.service.OrdenadorFacadeREST.class);
+        resources.add(entidades.service.PortatilFacadeREST.class);
+        resources.add(entidades.service.RecursoFacadeREST.class);
+        resources.add(entidades.service.RefuerzoFacadeREST.class);
+        resources.add(entidades.service.ReservaFacadeREST.class);
+        resources.add(entidades.service.RolFacadeREST.class);
+        resources.add(entidades.service.SalaFacadeREST.class);
+        resources.add(entidades.service.SistemaOperativoFacadeREST.class);
+        resources.add(entidades.service.UsuarioFacadeREST.class);
         resources.add(servicios.EmailAutomatizado.class);
     }
     
